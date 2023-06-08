03b35a550cf915d08fcc62f6bfd2910727efea44
vipin-verma <vipinrnverma@gmail.com>
2023-06-08 14:45:31 +0530

    Initial commit

---
+++ src/test/java/com/example/vvSpringKafka/VvSpringKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vvSpringKafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VvSpringKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
